ac3bc978e39475a7cd5d43e39066b28ae8427231
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-10-21 21:00:01 +0300

    a

---
+++ js/js.js
@@ -0,0 +1,32 @@
+// що таке змінна - коробочка в яку ми можемо зберігти будь-яке значення
+// Змінна збергіється в памʼяті
+// Для того щоб виділити памʼять під зберігання чого небудь вам необхідно знати два слова const та let
+
+// const - не змінюють своє значення
+const name = "Artem";
+
+let age = 23;
+
+age = age + 1;
+
+console.log(age);
+// .log() - дозволяє вивести в консоль повідомлення
+console.log("Hello world!");
+
+const multiply = 2 ** 10;
+
+console.log(multiply);
+
+// Типи даних
+
+//
+
+const string = "asdqwezxc";
+
+const number = 10;
+// Правда не правда , так або ні
+const boolean = true || false;
+
+console.log(1 < 10); // true
+
+console.log(1 > 10); // false
