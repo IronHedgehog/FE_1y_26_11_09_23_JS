ab017589c40578b792113e0e19f64a202d61a675
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2024-12-09 20:59:29 +0200

    a

--- js/js.js
+++ js/js.js
@@ -1,74 +1,67 @@
-// const button = document.getElementById("button");
-// 1- знайти елемент(отримати посилання на html елемент який вас цікавить)
-const button = document.querySelector(".button");
-const input = document.querySelector("#input");
-const form = document.querySelector(".form");
-const check = document.querySelector("#check");
-// .addEventListener - додати слухача події
-// 2 - Після того як ми знайшли елемент  ми маємо повісити на нього слухач події(addEventListener)
-button.addEventListener("click", onClick);
-// e - обʼєкт події
-let counter = 0;
-// 3- Створити функцію(обробник події) з вказівками як js має реагувати на цю подію
-function onClick(event) {
-  // target - це елемент на якому відбулась подія
-  console.log(event.target);
-  counter++;
+let pro = 100;
+let normal = 50;
+let elit = 500;
+let eli = 400;
+let el = 300;
 
-  if (counter === 1) {
-    button.removeEventListener("click", onClick);
-  }
-  console.log(`Я клікнув ${counter} разів`);
+let cost;
+const user = pro;
+
+if (user === pro) {
+  console.log("Підписка PRO");
+} else if (user === normal) {
+  console.log("Підписка норм");
+} else if (user === elit) {
+  console.log("Підписка elit");
+} else if (user === eli) {
+  console.log("Підписка eli");
+} else if (user === el) {
+  console.log("Підписка el");
+} else {
+  console.log("Ви не підписані");
 }
 
-// 1- знайти елемент(отримати посилання на html елемент який вас цікавить)
-// 2 - Після того як ми знайшли елемент  ми маємо повісити на нього слухач події(addEventListener)
-// 3- Створити функцію(обробник події) з вказівками як js має реагувати на цю подію
-// input - ввід тексту
-input.addEventListener("input", onInput);
+// key - це елемент для порівняння
+switch (user) {
+  case pro:
+    console.log("Підписка PRO");
+    break; // Зупинка виконання switch
 
-function onInput(e) {
-  const inputValue = e.target.value;
+  case normal:
+    console.log("Підписка normal");
 
-  console.log(inputValue);
+  case elit:
+    console.log("Підписка elit");
+
+  case eli:
+    console.log("Підписка eli");
+
+  case el:
+    console.log("Підписка el");
+
+  default:
+    console.log("У вас немає підписки");
 }
+// Змінні створені в глобальній області бачення доступні усюди
+// const a = 5;
 
-form.addEventListener("submit", onFormSubmit);
+// if (condition) {
+// }
 
-function onFormSubmit(e) {
-  // КОли ми відправляємо форму то сторінка за замовчуванням самотсійно оновлюється,щоб цьому запобігти ми використовуємо   e.preventDefault(); - що означає заблокувати поведінку за замовчуванням
-  e.preventDefault();
-  const emailValue = e.target.elements.email.value;
-  const passwordValue = e.target.elements.password.value;
-  console.log(emailValue);
-  console.log(passwordValue);
-}
-// Подія активації інтерактивного елементу
-input.addEventListener("focus", onFocus);
+// function name(params) {}
 
-function onFocus(e) {
-  input.style.border = "1px solid red";
-  input.style.outline = "none";
-}
-// blur - подія втрати фокусу
-input.addEventListener("blur", onBlur);
+// {} - блок у якому ми будемо зберігати код,
+{
+  // b - створено всередені блоку тому доступно лише всередині нього
+  const b = 5;
 
-function onBlur(e) {
-  const value = e.target.value;
-  if (value.length < 6) {
-    console.log("Пароль занадто слабкий");
-    input.style.border = "1px solid red";
-  } else {
-    input.style.border = "none";
-    input.style.outline = "1px solid black";
-  }
-}
-// 
-check.addEventListener("change", onChange);
+  console.log(b);
+  {
+    const a = 5;
+    console.log(a + b);
 
-function onChange(e) {
-  console.log(e.target.checked);
-  if (!e.target.checked) {
-    button.disabled = true;
+    {
+      console.log(a);
+    }
   }
 }
