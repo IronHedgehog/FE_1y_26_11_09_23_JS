1f010ef174fc5ce5fed3117a7e7b849df199d7a1
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-01-13 20:59:41 +0200

    a

--- js/index.js
+++ js/index.js
@@ -1,97 +1,69 @@
-getSum();
-// ab(5, 5);
-
-// function - створи(задекларуй) нам функцію
-// name - імʼя функції (англ.букв. get put patch post, )
-// (params) - це те що наша функція отримує ззовні для роботи
-//{} - тіло функції, де ми записуєм код який має виконатись при виклику
-
-const function2 = function () {
-  console.log("Початок виконання функції2");
+// колбек - функція яка сама по собі ніколи не відпрацьовує(сервісна функція)
+const getMaterials = function (money) {
+  console.log(`За такі кошти (${money}) ми пропунуємо такі матеріали`);
+};
+// колбек - функція яка сама по собі ніколи не відпрацьовує(сервісна функція яка викликається у інших функціях(функціях вищого порядку))
+const builders = function (money) {
+  console.log(`За такі кошти (${money}) ми пропунуємо таких робітників`);
 };
 
-// Функціональний вираз
-const function1 = function () {
-  console.log("Початок виконання функції1");
-  function2();
-  console.log("Продовження виконання коду функції1");
+// Функція вищого порядку(приймає параметрами інші функції)
+const buildHouse = function (money, getMaterials, builders) {
+  getMaterials(money);
 };
 
-console.log("Виконання основного коду");
+buildHouse(1000, getMaterials, builders);
 
-function1();
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//callback
+const filter = function (arr, userChoice) {
+  // const filteredArr = [];
 
-console.log(" Продовження виконнання основного коду");
-// a, b - параметри які ми отримуємо при виклиці нашої функції
-function function3(a = 0, b = 0) {
-  console.log(a);
-  console.log(b);
-  console.log(a + b);
-}
+  // return filteredArr;
 
-function3(10, 20);
-
-function hello(name) {
-  // return - завершує виконання функції та повертає її результат
-  if (typeof name === "undefined") {
-    console.log("Імʼя не вказано але привіт");
-    return;
-  }
-
-  // if (typeof name === "undefined") {
-  //   console.log(`Привіт, Anonym`);
+  // for (let i = 0; i < arr.length; i++) {
+  //   const element = arr[i];
+  //   if (element.incl) {
+  //     filteredArr.push(element);
+  //   }
   // }
-  console.log(`Привіт, ${name}`);
-}
 
-hello();
-
-function name(params) {
-  const a = 5;
-}
-
-const add = function () {
-  // arguments - дозволяє отримати всі аргументи які користувач відправив нам для обробки але у вигляді псевдомасиву
-
-  // Щоб перетворити псевдомасив в масив  Array.from()
-  const args = Array.from(arguments);
-  console.log(args);
+  return !arr.includes(userChoice);
 };
 
-add(1, 2, 3, 4, 4, 5, 6, 67, 78, 8, 9, 9, 10);
+const filterModified = (arr, userChoice) => !arr.includes(userChoice);
 
-function getSum(number1 = 0, number2 = 0) {
-  // typeof - повертає тип даних який зберігається у змінній
-  console.log(typeof number1);
-  console.log(typeof number2);
-  // якщо параметри числа то додаються як числа
-  if (typeof number1 === "number" && typeof number2 === "number") {
-    console.log(number1 + number2);
+// Функція вищого порядку
+const userInput = function (filter) {
+  const input = [1];
+  while (true) {
+    const userInput = prompt(
+      "Введіть значення. Якщо воно вже існує, його не буде додано."
+    );
+    if (userInput === null) break;
+    if (filter(input, userInput)) {
+      input.push(userInput);
+    } else {
+      console.log("Елемент вже є");
+    }
   }
-  // якщо хоча б один елемент є строкою ми додаєм їх як строки
-  if (typeof number1 === "string" || typeof number2 === "string") {
-    console.log(number1 + number2);
-  }
-
-  // console.log("number1: ", number1);
-  // console.log("number2: ", number2);
-  // console.log("sum: ", number1 + number2);
-}
-// Функціональний вираз
-const ab = function (material, robitnik) {
-  console.log(material + robitnik);
+  console.log(input);
 };
 
-ab("Derevo", false);
+// userInput(filter);
 
-console.log(Number("asd"));
+// const add = function (a, b, c) {
+//   console.log(a + b + c);
+// };
 
-console.log(prompt("null"));
+// add(1, 2, 3);
+// =>
+const add = (a, b, c) => {
+  return a + b + c;
+  // console.log(a - b - c);
+};
+add(1, 2, 3);
 
-console.log(typeof ab);
-// function declaration
-// function ab(a, b) {
-//   console.log(a + b);
-// }
+const t = (x) => console.log(x);
 
-// ab();
+const y = () => console.log(2);
