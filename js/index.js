d5a455b1fe84f9d9dacf22bcfa0f9258de5de0da
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2025-02-17 20:55:09 +0200

    a

--- js/index.js
+++ js/index.js
@@ -1,115 +1,159 @@
-// impera - рецепт фільтрації масиву
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
 
-const numbers = [12, 3, 45, 2, 3, 46];
-const filteredArray = [];
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
+  },
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
 
-for (let i = 0; i < numbers.length; i++) {
-  const element = numbers[i];
-  if (element < 20) {
-    filteredArray.push(element);
-  }
-}
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
+  },
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
 
-// console.log(filteredArray);
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
 
-// Declaration code - Замовлення готового результату
-const declarationNumbers = [100, 200, 300, 400, 1, 2, 3, 4, 1012, 20102];
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
 
-const declarationFilterNumbers = declarationNumbers.filter((number) => {
-  return number < 1000;
-});
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
 
-// console.log(declarationFilterNumbers);
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
 
-// Функції з побічними ефектами(Видозмінює початковий вигляд параметрів які ми передаєм)
-// console.log("ДО : ", numbers);
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
 
-const dirtyAdd = (arr, value) => {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i] + value;
-  }
-  return arr;
-};
+const numbers = [1, 2, 3, 4, 5];
 
-// console.log(dirtyAdd(numbers, 10));
+// 0 - акумулятор
+console.log(
+  numbers.reduce((acc, number) => {
+    console.log(acc);
+    console.log(number);
+    return acc + number;
+  }, 0)
+);
 
-// console.log("Після :", numbers);
+console.log(
+  users.reduce((acc, user) => {
+    return acc + user.balance;
+  }, 0)
+);
 
-// чиста функція(функція,що не видозмінює початкові дані)
+console.log(
+  numbers.reduce((acc, number, idx) => {
+    // push - повертає довжину оновленого масиву після додавання
+    // acc.push(idx);
+    // return acc
+    console.log(acc);
+    acc.push(idx);
+    return acc;
+  }, [])
+);
 
-const pureAdd = (arr, value) => {
-  const addedArr = [];
+// Метод reduce - може збирати, може збирати обʼєкти(Він може замінити всі перебираючі методи масиву)
 
-  for (let i = 0; i < arr.length; i++) {
-    addedArr.push(arr[i] + value);
-  }
-  return addedArr;
-};
+// Метод sort
 
-// console.log("pureAdd :", pureAdd(declarationNumbers, 1000));
+const numbers2 = [5, 3, 2, 4, 1, 0, 7, 6, 10, 8, 9, 11, 12, 13, 14, 21, 35, 46];
 
-// console.log("default:", declarationNumbers);
+console.log(numbers2.sort());
+console.log(
+  numbers2.sort((number1, number2) => {
+    return number2 - number1;
+  })
+);
 
-const arr = [1, 2, 3, 4, 5];
-// функція з домішками (псує початкові дані)
-function dirty(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = arr[i] / 2;
-  }
+console.log(
+  numbers2.sort((number1, number2) => {
+    return number1 - number2;
+  })
+);
 
-  return arr;
-}
+const names = ["Artem", "Yuna", "Olexandr", "Max", "Nikita"];
 
-// console.log("Результат роботи функції з домішками ", dirty(arr));
+console.log(names.sort());
 
-// console.log("Початковий масив", arr);
+const abc = ["b", "c", "w", "r"];
 
-function pure(arr) {
-  const result = [];
+console.log(
+  abc.sort((a, b) => {
+    return a.localeCompare(b);
+  })
+);
 
-  for (let i = 0; i < arr.length; i++) {
-    result.push(arr[i] * 2);
-  }
-  console.log(result);
-  return result;
-}
-
-// console.log("Результат роботи чистої функції ", pure(arr));
-
-// console.log("Початковий масив", arr);
-
-// Перебираючі методи масиву
-
-// foreach
-
-const arr1 = [1, 2, 3, 4, 5];
-
-for (let i = 0; i < arr1.length; i++) {
-  console.log(arr1[i]);
-}
-
-arr1.forEach((number, index) => {
-  console.log(number, index);
-});
-
-const multi = arr1.map((number) => {
-  return number / 2;
-});
-
-console.log(multi);
-
-console.log(arr1);
-
-const arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const filterArr3 = arr3.filter((number) => {
-  return number < 9;
-});
-
-console.log(filterArr3);
-
-const findTen = arr3.find((number) => {
-  return number === 11;
-});
-
-console.log(findTen);
+console.table(
+  users
+    .filter((user) => user.eyeColor === "brown")
+    .sort((a, b) => b.balance - a.balance)
+    .map((user) => user.name)
+);
